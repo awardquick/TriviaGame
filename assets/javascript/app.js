e96955af80beb70cc19b90f62ad4f6422e497ef6
Kismet <47130796+charlotterice@users.noreply.github.com]>
2019-03-26 21:27:29 -0400

    just for fun after close call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,58 @@
-var userInput = []
-
-
-//setTimeout(next, 5000);
-
-$("#start-button").on("click",beginGame);
-$("#start-button").on("click",startDisappear);
-function startDisappear(){
-   $("#start-button").hide();
-   $("#next-button").show();
+var userInput = [];
+var answerOptions ={
+ correct = 0,
+incorrect = 0,
 }
-function beginGame () {
-    $("#box").html("<h3>Question 1</h3>");
-    console.log("This is question 1");
-    $("#question-text").html("<div>This is the first question</div>");
-    console.log("this is working now");
-   $("#next-button").on("click",question);
+var questionCounter = 0;
+var count=30;
+var questionArray = [{
+    question: "This is a question",
+    options: ["A1", "A2", "A3", "A4"],
+    correctAnswer: 1,
+}, {
+    question: "This is question 2",
+    options: ["A1", "A2", "A3", "A4"],
+    correctAnswer: 0,
+}, {
+    question: "This is question number 3",
+    options: ["A1", "A2", "A3", "A4"],
+    correctAnswer: 3
+}];
+
+$("#start-button").on("click", displayQuestions);
+$("#start-button").on("click", startGame);
+
+function startGame() {
+    $("#start-button").hide();
+    $("#next-button").show();
+    counter = setInterval(timer, 3000);
+    displayQuestions();
+};
+
+function timer() {
+    count--;
+    if (count <= 0) {
+        clearInterval(counter);
+        return;
+    }
+    $("#time-remaining").html("Time Remaining: " + count + "seconds");
 }
-function question() {
-    $("#box").html("<h3> Question 2 <h3>");
-    $("#question-text").html("<div>This is the second question</div>");
+function displayQuestions() {
+    $("#start-text").hide();
+    $("#question-text").html("<h3>" + questionArray[0].question + "</h3>");
+    var choicesArr = questionArray[0].choices;
+    var userInputArray = [];
+    for (i =0; i <choicesArr.length; i ++){
+        var answer = $("#choices-text");
+        answer.text(choicesArr[i]);
+        answer.attr('data-id',i);
+        $("#choices-text").append (answer);
+    }
+    $("#next-button").on("click", displayQuestions);
 }
+// function timesUp (){
+//     questionCounter++;
+//     newQuestion();
+//     setTimeout(timesUp,3000);
+// }
+//   
